bcc2dacad80395bbbab7c23cd8361d038386b4b4
armen <armenpetrosan319@gmail.com>
2026-08-31 09:04:04 +0300

    created api tests by products endpoints

--- app/tests/integrations/conftest.py
+++ app/tests/integrations/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from app.models.product import Product
 from app.models.category import Category
+from app.schemas.product import ProductCreate, ProductUpdate
 
 @pytest.fixture
 async def create_category(test_async_session):
@@ -18,6 +19,7 @@ async def create_product(test_async_session, create_category):
     test_async_session.add(product)
     await test_async_session.commit()
     await test_async_session.refresh(product)
+    return product
 
 @pytest.fixture
 async def create_products(test_async_session, create_category):
@@ -36,3 +38,47 @@ async def create_products(test_async_session, create_category):
     print("ТЕСТОВЫЕ ДАННЫЕ====================")
     print(products)
     return products
+
+@pytest.fixture
+def schema_product_create_valid(create_category):
+    return ProductCreate(
+        title = "Вилка", description = "Удобная вилка", 
+        price = 100, quantity = 3, category_id = create_category.id)
+
+@pytest.fixture
+def schema_product_create_not_valid():
+    return {
+            "title":  "   ", 
+            "description": "a"*101, 
+            "price": 0, 
+            "quantity": -56, 
+            "category_id": -5
+        }
+    
+@pytest.fixture
+def schema_product_create_not_found_category():
+    return ProductCreate(
+            title = "Вилка", description = "Удобная вилка", 
+            price = 100, quantity = 3, category_id = 101)
+
+@pytest.fixture
+def schema_product_update_valid():
+    return ProductUpdate(
+        title = "Ложка", description = "Удобная ложка", 
+        price = 200, quantity = 1)
+
+@pytest.fixture
+def schema_product_update_not_valid():
+    return {
+        "title":  "   ", 
+        "description": "a"*101, 
+        "price": 0, 
+        "quantity": -56, 
+        "category_id": -5
+    }
+
+@pytest.fixture
+def schema_product_update_not_found_category():
+    return ProductUpdate(
+            title = "Вилка", description = "Удобная вилка", 
+            price = 100, quantity = 3, category_id = 101)
